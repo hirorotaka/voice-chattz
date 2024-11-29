3475c92941d27d61de33caa5ff188bce254b22dd
hiroki takada <hirorotaka5@gmail.com>
2024-11-29 11:44:05 +0900

    Threadテーブルの初期雛形作成

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreThreadRequest;
+use App\Http\Requests\UpdateThreadRequest;
+use App\Models\Thread;
+use Illuminate\Validation\Rules\In;
+use Inertia\Inertia;
+use Inertia\Response as InertiaResponse;
+
+class ThreadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): InertiaResponse
+    {
+
+        return Inertia::render('top');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreThreadRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateThreadRequest $request, Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Thread $thread)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/top.tsx
@@ -0,0 +1,10 @@
+import { Head } from "@inertiajs/react";
+
+export default function Top() {
+    return (
+        <>
+            <Head title="top" />
+            <h1>top</h1>
+        </>
+    );
+}
