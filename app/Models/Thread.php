1bf8275e140ed8ba603fb9f53d75879c4fe97ef6
hiroki takada <hirorotaka5@gmail.com>
2024-12-01 14:23:32 +0900

    user・threadテーブルのリレーション実施

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -15,6 +15,11 @@ class Thread extends Model
         return $this->hasMany(Message::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     protected  $fillable = [
         'title',
     ];
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
 }
