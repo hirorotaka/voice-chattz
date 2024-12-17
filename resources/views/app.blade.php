f475b33592e7cb2284f0a7200c7ed2036e35bac5
hiroki takada <hirorotaka5@gmail.com>
2024-12-17 15:54:03 +0900

    フォントをgooglefontに変更

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,8 +8,10 @@
   <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
   <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
+    as="style">
 
   <!-- Styles -->
   <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
