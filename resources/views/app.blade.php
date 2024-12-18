35124c0a51bdc4c5125c2e2b6490ed6c0de0cb7f
hiroki takada <hirorotaka5@gmail.com>
2024-12-18 23:16:27 +0900

    モバイル用のセーフエリア対応(safari対応)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,10 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport"
-    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
+  {{-- <meta name="viewport"
+    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"> --}}
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=contain">
+  <meta name="apple-mobile-web-app-capable" content="no">
 
   <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
