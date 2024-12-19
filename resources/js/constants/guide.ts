5cb619e1854098ed023623a8d3b6fd27115de75a
hiroki takada <hirorotaka5@gmail.com>
2024-12-19 13:29:08 +0900

    使い方ガイドの写真差し替え

--- resources/js/constants/guide.ts
+++ resources/js/constants/guide.ts
@@ -7,7 +7,7 @@ export const slides: SlideType[] = [
         description:
             "サイドバーの「+新規スレッド作成」をクリック。\n学習したい言語とAIの役割を選んで会話を始めましょう。",
         image: {
-            webp: `${AWS_CLOUDFRONT_URL}/step1_var3.webp`,
+            webp: `${AWS_CLOUDFRONT_URL}/step1_var4.webp`,
             fallback: "/step1.png",
         },
     },
@@ -16,7 +16,7 @@ export const slides: SlideType[] = [
         description:
             "画面下部のマイクボタンをクリックして録音開始。\n赤色に変わったら、話しかけてみましょう。",
         image: {
-            webp: `${AWS_CLOUDFRONT_URL}/step2_var3.webp`,
+            webp: `${AWS_CLOUDFRONT_URL}/step2_var4.webp`,
             fallback: "/step2.png",
         },
     },
@@ -25,7 +25,7 @@ export const slides: SlideType[] = [
         description:
             "話し終わったら、もう一度マイクボタンをクリック。\nあなたの声が、AIキャラクターに送信されます。",
         image: {
-            webp: `${AWS_CLOUDFRONT_URL}/step3_var3.webp`,
+            webp: `${AWS_CLOUDFRONT_URL}/step3_var4.webp`,
             fallback: "/step3.png",
         },
     },
@@ -34,7 +34,7 @@ export const slides: SlideType[] = [
         description:
             "AIキャラクターが音声で応答します。\n右下のスライダーで音声の速度を調整できます。\n必要な場合は、日本語訳も表示できます。",
         image: {
-            webp: `${AWS_CLOUDFRONT_URL}/step4_var3.webp`,
+            webp: `${AWS_CLOUDFRONT_URL}/step4_var4.webp`,
             fallback: "/step4.png",
         },
     },
@@ -43,7 +43,7 @@ export const slides: SlideType[] = [
         description:
             "「マイAIキャラクター」をクリック。\n自分だけのAIキャラクターを作成できます。\n作ったAIは「公開」をオンにすると、他のユーザーにもシェアできます。",
         image: {
-            webp: `${AWS_CLOUDFRONT_URL}/step5_var3.webp`,
+            webp: `${AWS_CLOUDFRONT_URL}/step5_var4.webp`,
             fallback: "/step5.png",
         },
     },
@@ -52,7 +52,7 @@ export const slides: SlideType[] = [
         description:
             "「公開AIキャラクター」をクリック。\n「公開中のものを使用する」をオンにすると、\n他のユーザーが作成したAIキャラクターを使えます。\n役割はスレッドを作成するときに選べるようになります。",
         image: {
-            webp: `${AWS_CLOUDFRONT_URL}/step6_var3.webp`,
+            webp: `${AWS_CLOUDFRONT_URL}/step6_var4.webp`,
             fallback: "/step6.png",
         },
     },
@@ -61,7 +61,7 @@ export const slides: SlideType[] = [
         description:
             "ブラウザからマイクのアクセス許可のポップアップが出る方は、許可をクリックしてください。\n拒否を押してしまった方は、URL欄左端にあるアイコンをクリックしてマイクの許可をしてください。",
         image: {
-            webp: `${AWS_CLOUDFRONT_URL}/step7_var3.webp`,
+            webp: `${AWS_CLOUDFRONT_URL}/step7_var4.webp`,
             fallback: "/step7.png",
         },
     },
