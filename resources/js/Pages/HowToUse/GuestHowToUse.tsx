aa9a6f76ebcd766e3952b775d41376f64d2b4a21
hiroki takada <hirorotaka5@gmail.com>
2024-12-20 12:19:56 +0900

    使い方ガイドのレイアウト修正

--- resources/js/Pages/HowToUse/GuestHowToUse.tsx
+++ resources/js/Pages/HowToUse/GuestHowToUse.tsx
@@ -7,11 +7,9 @@ export default function GuestHowToUse() {
     return (
         <GuestAppLayout title="使い方ガイド">
             <div className="container mx-auto px-1 sm:p-2 md:p-4 lg:px-6 ">
-                <div className="flex items-center justify-center mb-4">
-                    <h1 className="text-white text-lg sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center">
-                        使い方ガイド
-                    </h1>
-                </div>
+                <h1 className="mb-4 text-white text-lg sm:text-3xl md:text-4xl lg:text-5xl font-bold">
+                    使い方ガイド
+                </h1>
                 <GuideSlider slides={slides} />
             </div>
         </GuestAppLayout>
