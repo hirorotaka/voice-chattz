1d2a1749db727a65ee95558372ec196bb91d7c66
hiroki takada <hirorotaka5@gmail.com>
2024-12-25 09:41:39 +0900

    利用規約・プライバシーポリシーのフォーム閉じるボタンを追加

--- resources/js/Components/Utils/LegalModal.tsx
+++ resources/js/Components/Utils/LegalModal.tsx
@@ -1,4 +1,5 @@
 import Modal from "@/Components/Modal";
+import { IoCloseOutline } from "react-icons/io5";
 
 interface LegalModalProps {
     onClose: () => void;
@@ -13,8 +14,16 @@ export default function LegalModal({
 }: LegalModalProps) {
     return (
         <Modal show={show} onClose={onClose}>
-            <div className="mt-2 max-h-[85vh] overflow-y-auto">
-                {modalContent}
+            <div className="relative">
+                <button
+                    onClick={onClose}
+                    className="absolute right-2 top-2 p-2 rounded-full hover:bg-gray-100 transition-colors"
+                >
+                    <IoCloseOutline className="h-6 w-6 text-gray-500" />
+                </button>
+                <div className="mt-2 max-h-[85vh] overflow-y-auto pt-8">
+                    {modalContent}
+                </div>
             </div>
         </Modal>
     );
