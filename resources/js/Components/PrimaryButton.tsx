5c1dc0b7321c5464570a49fdb530858310ed5b71
hiroki takada <hirorotaka5@gmail.com>
2025-02-15 21:54:49 +0900

    プライマリボタンのdisable時の挙動修正

--- resources/js/Components/PrimaryButton.tsx
+++ resources/js/Components/PrimaryButton.tsx
@@ -1,7 +1,7 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes } from "react";
 
 export default function PrimaryButton({
-    className = '',
+    className = "",
     disabled,
     children,
     ...props
@@ -9,11 +9,19 @@ export default function PrimaryButton({
     return (
         <button
             {...props}
-            className={
-                `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
-                    disabled && 'opacity-25'
-                } ` + className
-            }
+            className={`
+                inline-flex items-center justify-center gap-2 rounded-md
+                border border-transparent bg-gray-800 px-4 py-2
+                text-xs font-semibold uppercase tracking-widest text-white
+                transition duration-150 ease-in-out
+                focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+                ${
+                    disabled
+                        ? "cursor-not-allowed opacity-50"
+                        : "hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900"
+                }
+                ${className}
+            `}
             disabled={disabled}
         >
             {children}
